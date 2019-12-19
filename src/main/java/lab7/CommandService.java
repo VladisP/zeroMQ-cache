c29416bbfb33ccbf5826a876a80c09e23aa65982
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-12-19 13:00:28 +0300

    Scripted auto-commit on change (2019-12-19 13:00:28) by gitwatch.sh

--- src/main/java/lab7/CommandService.java
+++ src/main/java/lab7/CommandService.java
@@ -12,6 +12,6 @@ public class CommandService {
     }
 
     public static CommandType getCommandType(String cmd) {
-        Pattern pattern = 
+        Pattern pattern = Pattern.compile("SET \d");
     }
 }
