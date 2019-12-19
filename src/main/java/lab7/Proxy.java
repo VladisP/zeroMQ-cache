8330e52401f0c3a60da19063fe10c2ea7179479f
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-12-19 18:49:43 +0300

    Scripted auto-commit on change (2019-12-19 18:49:43) by gitwatch.sh

--- src/main/java/lab7/Proxy.java
+++ src/main/java/lab7/Proxy.java
@@ -1,12 +1,14 @@
 package lab7;
 
+import org.zeromq.ZMQ;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Proxy {
 
     private static final List<StorageInfo> storages = new ArrayList<>();
-    private 
+    private static ZMQ.Socket
 
     public static void main(String[] args) {
 
