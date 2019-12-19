565424d16bd94d123d851ff4abfe4ae3bf20c6ee
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-12-19 13:59:21 +0300

    Scripted auto-commit on change (2019-12-19 13:59:21) by gitwatch.sh

--- src/main/java/lab7/Storage.java
+++ src/main/java/lab7/Storage.java
@@ -9,7 +9,7 @@ public class Storage {
     public static void main(String[] args) {
         ZContext context = new ZContext();
         ZMQ.Socket socket = context.createSocket(SocketType.DEALER);
-        
+        socket.connect()
 
         int startCell = Integer.parseInt(args[0]);
         int endCell = Integer.parseInt(args[1]);
