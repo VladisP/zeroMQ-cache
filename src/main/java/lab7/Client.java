12949fe51a49a57d56e3ec125a329dc963bcd93e
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-12-19 12:39:00 +0300

    Scripted auto-commit on change (2019-12-19 12:39:00) by gitwatch.sh

--- src/main/java/lab7/Client.java
+++ src/main/java/lab7/Client.java
@@ -6,11 +6,11 @@ import org.zeromq.ZMQ;
 
 public class Client {
 
-    private final static String CLIENT_ADDRESS
+    private final static String CLIENT_ADDRESS = "tcp://localhost:5555";
 
     public static void main(String[] args) {
         ZContext context = new ZContext();
         ZMQ.Socket socket = context.createSocket(SocketType.REQ);
-        socket.connect("tcp://localhost:5555");
+        socket.connect(CLIENT_ADDRESS);
     }
 }
