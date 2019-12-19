6759ae46be49f9b792b188f35d412eb3629734c2
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-12-19 12:41:22 +0300

    Scripted auto-commit on change (2019-12-19 12:41:22) by gitwatch.sh

--- src/main/java/lab7/Client.java
+++ src/main/java/lab7/Client.java
@@ -4,6 +4,8 @@ import org.zeromq.SocketType;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
 
+import java.util.Scanner;
+
 public class Client {
 
     private final static String CLIENT_ADDRESS = "tcp://localhost:5555";
@@ -11,6 +13,7 @@ public class Client {
     public static void main(String[] args) {
         ZContext context = new ZContext();
         ZMQ.Socket socket = null;
+        Scanner in = new Scanner(System.in);
 
         try {
             socket = context.createSocket(SocketType.REQ);
