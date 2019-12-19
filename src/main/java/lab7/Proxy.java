85fb25844703d5a8967d61e0fba9055734f70107
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-12-19 19:12:53 +0300

    Scripted auto-commit on change (2019-12-19 19:12:53) by gitwatch.sh

--- src/main/java/lab7/Proxy.java
+++ src/main/java/lab7/Proxy.java
@@ -34,6 +34,7 @@ public class Proxy {
             if (items.pollin(1)) {
                 ZMsg msg = ZMsg.recvMsg(backend);
                 ZFrame address = msg.unwrap();
+                String id = address.getData()
             }
         }
     }
