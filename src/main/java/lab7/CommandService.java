6fa369904b7575161c97b5e0f054e7f5a4a85181
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-12-19 13:03:28 +0300

    Scripted auto-commit on change (2019-12-19 13:03:28) by gitwatch.sh

--- src/main/java/lab7/CommandService.java
+++ src/main/java/lab7/CommandService.java
@@ -4,6 +4,10 @@ import java.util.regex.Pattern;
 
 public class CommandService {
 
+    private static Pattern SET_COMMAND_PATTERN = Pattern.compile("SET \\d+ \\d+");
+    private static Pattern GET_COMMAND_PATTERN = Pattern.compile("GET \\d+");
+    private static Pattern EXIT_COMMAND_PATTERN = Pattern.compile();
+
     public static enum CommandType {
         GET,
         SET,
