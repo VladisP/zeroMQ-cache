0c37a14e3028b0d29a5217e45915237c17f50896
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-12-19 12:17:42 +0300

    Scripted auto-commit on change (2019-12-19 12:17:42) by gitwatch.sh

--- src/main/java/lab7/Client.java
+++ src/main/java/lab7/Client.java
@@ -1,10 +1,12 @@
 package lab7;
 
 import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
 
 public class Client {
 
     public static void main(String[] args) {
         ZContext context = new ZContext();
+        ZMQ.
     }
 }
