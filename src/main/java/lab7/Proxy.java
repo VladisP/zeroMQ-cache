e01784a6083c6af7fc207e9ccc699f82bfbbdf99
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-12-19 18:49:37 +0300

    Scripted auto-commit on change (2019-12-19 18:49:37) by gitwatch.sh

--- src/main/java/lab7/Proxy.java
+++ src/main/java/lab7/Proxy.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class Proxy {
 
     private static final List<StorageInfo> storages = new ArrayList<>();
+    private 
 
     public static void main(String[] args) {
 
