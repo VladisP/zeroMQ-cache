2619f7924794995598f954614994723640bcd9f0
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-12-19 18:15:17 +0300

    Scripted auto-commit on change (2019-12-19 18:15:17) by gitwatch.sh

--- src/main/java/lab7/Storage.java
+++ src/main/java/lab7/Storage.java
@@ -26,7 +26,7 @@ public class Storage {
         int startCell = Integer.parseInt(args[0]);
         int endCell = Integer.parseInt(args[1]);
 
-        long  System.currentTimeMillis()
+        long heartbeat System.currentTimeMillis()
         sendConnectCommand(socket, startCell, endCell);
 
         System.out.println("Хранилище настроено");
