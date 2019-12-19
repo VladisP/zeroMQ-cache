32bc4de528ed4e30fc0b3db0afcffd2c02de74e7
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-12-19 13:14:09 +0300

    Scripted auto-commit on change (2019-12-19 13:14:08) by gitwatch.sh

--- src/main/java/lab7/Client.java
+++ src/main/java/lab7/Client.java
@@ -12,6 +12,13 @@ public class Client {
 
     private final static String CLIENT_ADDRESS = "tcp://localhost:5555";
 
+    private static void printHelpMessage() {
+        System.out.println("Неверная команда, доступны только следующие команды:");
+        System.out.println("SET <key> <value>");
+        System.out.println("GET <key>");
+        System.out.println("F");
+    }
+
     public static void main(String[] args) {
         ZContext context = new ZContext();
         ZMQ.Socket socket = null;
@@ -21,12 +28,19 @@ public class Client {
             socket = context.createSocket(SocketType.REQ);
             socket.connect(CLIENT_ADDRESS);
 
+            System.out.println("Клиент готов к работе, для выхода press F");
+
             while (true) {
                 String cmd = in.nextLine();
                 CommandType cmdType = CommandService.getCommandType(cmd);
 
                 if (cmdType == CommandType.EXIT) {
                     break;
+                }
+
+                if (cmdType == CommandType.INVALID) {
+                    printHelpMessage();
+                    continue;
                 }
 
                 
