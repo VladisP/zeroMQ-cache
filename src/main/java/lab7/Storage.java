02c106c9261b66df2c0fe82ec3e0207faf16a581
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-12-19 18:13:44 +0300

    Scripted auto-commit on change (2019-12-19 18:13:44) by gitwatch.sh

--- src/main/java/lab7/Storage.java
+++ src/main/java/lab7/Storage.java
@@ -27,6 +27,7 @@ public class Storage {
         int endCell = Integer.parseInt(args[1]);
 
         sendConnectCommand(socket, startCell, endCell);
+        
 
         System.out.println("Хранилище настроено");
 
