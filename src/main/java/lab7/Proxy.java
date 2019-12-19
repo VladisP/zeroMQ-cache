51514192888caf9a2939fbccbe4ece4c999c32b9
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-12-19 18:48:53 +0300

    Scripted auto-commit on change (2019-12-19 18:48:53) by gitwatch.sh

--- src/main/java/lab7/Proxy.java
+++ src/main/java/lab7/Proxy.java
@@ -1,8 +1,10 @@
 package lab7;
 
+import java.util.List;
+
 public class Proxy {
 
-    private static final 
+    private static final List<StorageInfo> 
 
     public static void main(String[] args) {
 
