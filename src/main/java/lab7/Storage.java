3d6a189839f35bf4ac2c6e5e677c43c213786fa1
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-12-19 13:58:00 +0300

    Scripted auto-commit on change (2019-12-19 13:58:00) by gitwatch.sh

--- src/main/java/lab7/Storage.java
+++ src/main/java/lab7/Storage.java
@@ -3,6 +3,7 @@ package lab7;
 public class Storage {
 
     public static void main(String[] args) {
-        int startCell
+        int startCell = Integer.parseInt(args[0]);
+        int endCell = Integer.parseInt(args[1]);
     }
 }
