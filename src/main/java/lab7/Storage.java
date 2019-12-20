eb7ae689824c70326f291da254a3d31b867fb7ad
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-12-20 15:10:50 +0300

    Scripted auto-commit on change (2019-12-20 15:10:50) by gitwatch.sh

--- src/main/java/lab7/Storage.java
+++ src/main/java/lab7/Storage.java
@@ -44,7 +44,7 @@ public class Storage {
             ZMsg msg = ZMsg.recvMsg(socket, false);
 
             if (msg != null) {
-                String cmd = new String(msg.getLast())
+                String cmd = new String(msg.getLast().getData(), )
                 CommandType cmdType = CommandService.getCommandType(cmd);
 
                 if (cmdType == CommandType.GET) {
