0e3a50ad227469ffe8e64624afa52b7293b7a273
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-12-20 17:07:47 +0300

    Scripted auto-commit on change (2019-12-20 17:07:47) by gitwatch.sh

--- src/main/java/lab7/Client.java
+++ src/main/java/lab7/Client.java
@@ -47,7 +47,7 @@ public class Client {
                 socket.send(cmd, 0);
 //                String reply = socket.recvStr(0);
                 ZMsg msg = ZMsg.recvMsg(socket);
-                System.out.println(msg.getFirst().getData());
+                System.out.println(msg.getFirst().getData(), ZMQ.CHARSET);
             }
         } finally {
             context.destroySocket(socket);
