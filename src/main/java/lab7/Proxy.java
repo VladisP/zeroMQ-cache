efe44b672f1d2d06965d4341af32f2fcb13ba836
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-12-20 14:27:45 +0300

    Scripted auto-commit on change (2019-12-20 14:27:45) by gitwatch.sh

--- src/main/java/lab7/Proxy.java
+++ src/main/java/lab7/Proxy.java
@@ -74,7 +74,7 @@ public class Proxy {
                 }
             }
 
-            removeDeadStorages();
+            //removeDeadStorages();
         }
 
         context.destroySocket(frontend);
